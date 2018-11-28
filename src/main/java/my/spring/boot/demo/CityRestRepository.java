3ab67c69da369b6ec403a8a1015eae4818fcb72a
Evgeniy Bulanov <jeak110@gmail.com>
2018-11-28 11:25:14 +0700

    spring data rest

--- src/main/java/my/spring/boot/demo/CityRestRepository.java
+++ src/main/java/my/spring/boot/demo/CityRestRepository.java
@@ -1,8 +1,12 @@
 package my.spring.boot.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "city", collectionResourceRel = "city")
 public interface CityRestRepository extends JpaRepository<City, Long> {
+    List<City> getCitiesByCityLike(@Param("city") String city);
 }
